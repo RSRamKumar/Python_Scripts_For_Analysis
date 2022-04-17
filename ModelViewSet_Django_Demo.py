865b470071d9a40777e63e7e95f55141fb6ab622
Ram Kumar R S <39699070+RSRamKumar@users.noreply.github.com>
2022-04-17 15:09:54 +0200

    Create ModelViewSet_Django_Demo.py

---
+++ ModelViewSet_Django_Demo.py
@@ -0,0 +1,36 @@
+
+# method 1
+class GetVariantMetaDataUsingHGVScDNA(viewsets.ModelViewSet):
+ """ViewSet for the GetVariantMetaDataUsingHGVScDNA class"""
+ 
+    #models.Variant_Transcript.objects.filter(hgvs_cdna='NM_000059.4:c.68_69delAG' )
+    queryset = models.Variant_Transcript.objects.all()
+    serializer_class = serializers.VariantMetaDataSerializer
+    lookup_field = 'hgvs_cdna'
+    lookup_value_regex = "NM.+" #"[^/]+" , 'NM_\d+.\d+:.+' , '.+'
+    permission_classes = [permissions.IsAuthenticated]
+
+
+ # method 2
+from rest_framework.response import Response
+
+class GetVariantMetaDataUsingHGVScDNA(viewsets.ModelViewSet):
+    
+      #queryset = ''
+      lookup_value_regex =  '.+'
+      serializer_class = serializers.VariantMetaDataSerializer
+
+      def get_queryset(self):
+          return models.Variant_Transcript.objects.all()
+
+      def retrieve(self, request, *args, **kwargs):
+        
+        params = kwargs
+        print(params)
+
+        response = models.Variant_Transcript.objects.filter(hgvs = params['pk'])
+        serializer = serializers.VariantMetaDataSerializer(response, many=True)
+        return  Response(serializer.data)
+ 
+  
+#https://www.youtube.com/watch?v=ws0jwg1J0BU
